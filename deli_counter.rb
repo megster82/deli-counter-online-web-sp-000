168c518889487eca68d5545b5843d01864a191eb
meaghan jennings <meaghanljennings@gmail.com>
2021-03-01 20:33:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
-    line = "The line is currently: "
+    line = "The line is currently:"
     katz_deli.each_with_index do |value, index|
       line << " #{index + 1}. #{value}"
     end
